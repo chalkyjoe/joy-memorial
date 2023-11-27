3b7fe19706686094aecdba3a72b09abc52bdb1c6
jack.brown <jack.hoyle@autocab.com>
2023-11-27 10:23:52 +0000

    Use Sql Server

--- joy-memorial/Program.cs
+++ joy-memorial/Program.cs
@@ -11,7 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ??
                        throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(connectionString));
+    options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddRoles<IdentityRole>()
@@ -25,7 +25,7 @@ builder.Services.AddScoped<StoryRepository>();
 builder.Services.AddScoped<MediaRepository>();
 
 var app = builder.Build();
-
+var scope = app.Services.CreateScope();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -49,5 +49,33 @@ app.UseAuthorization();
 app.MapControllers();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
+var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
 
+string roleName = "Admin";
+IdentityResult roleResult;
+
+// Check if the role exists, create it if not
+var roleExist = await roleManager.RoleExistsAsync(roleName);
+if (!roleExist)
+{
+    // Create the role
+    roleResult = await roleManager.CreateAsync(new IdentityRole(roleName));
+}
+
+// Create an admin user
+IdentityUser user = await userManager.FindByEmailAsync("admin@example.com");
+
+if (user == null)
+{
+    user = new IdentityUser()
+    {
+        UserName = "admin@example.com",
+        Email = "admin@example.com",
+    };
+    await userManager.CreateAsync(user, "Admin@123");
+}
+
+// Assign the admin user to the Admin role
+await userManager.AddToRoleAsync(user, roleName);
 app.Run();
